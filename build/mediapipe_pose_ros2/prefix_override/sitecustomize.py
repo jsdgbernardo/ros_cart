c5d0c04c3ee0fc799b4017ecb9fbd6b120073cde
Jonathan <bernardojonathansimon@gmail.com>
2025-07-02 15:36:07 +0800

    fixed git ignore

--- build/mediapipe_pose_ros2/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jonathanb/ros_cart/install/mediapipe_pose_ros2'
--- build/mediapipe_pose_ros2/share/mediapipe_pose_ros2/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/jonathanb/ros_cart/build/mediapipe_pose_ros2"
