1365813d31f9b9b371c221ea93e3819be9e113b4
Jonathan <bernardojonathansimon@gmail.com>
2025-07-02 14:29:49 +0800

    fixed colcon build warnings

--- src/mediapipe_cam/setup.py
+++ src/mediapipe_cam/setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup
+import os
 
 package_name = 'mediapipe_cam'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.0',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='your_name',
-    maintainer_email='your_email@example.com',
-    description='ROS 2 node to stream from phone camera using IP Webcam for MediaPipe input',
-    license='MIT',
+    maintainer='jsdgbernardo',
+    maintainer_email='bernardojonathansimon@gmail.com',
+    description='Publishes camera data from phone',
+    license='License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'phone_cam_node = mediapipe_cam.phone_cam_node:main',
+            'pose_node = mediapipe_cam.pose_node:main',
         ],
     },
 )
--- src/mediapipe_pose_ros2/setup.py
+++ src/mediapipe_pose_ros2/setup.py
@@ -4,13 +4,19 @@ package_name = 'mediapipe_pose_ros2'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.0.0',
     packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', [
+            f'resource/{package_name}']),
+        (f'share/{package_name}', ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jsdgbernardo',
     maintainer_email='bernardojonathansimon@gmail.com',
-    description='ROS 2 node that integrates MediaPipe Pose estimation.',
+    description='ROS 2 wrapper for MediaPipe pose detection',
+    license='License',
     entry_points={
         'console_scripts': [
             'pose_node = mediapipe_pose_ros2.pose_node:main',
