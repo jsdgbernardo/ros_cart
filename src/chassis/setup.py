b555d2d68334bdb6902479cbcec656f407971d4c
Jonathan <bernardojonathansimon@gmail.com>
2025-07-04 17:57:32 +0800

    added cart xacro model

---
+++ src/chassis/setup.py
@@ -0,0 +1,28 @@
+from glob import glob
+import os
+from setuptools import setup
+
+package_name = 'chassis'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'description'), glob('description/*.xacro')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jonathanb',
+    maintainer_email='bernardojonathansimon@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
