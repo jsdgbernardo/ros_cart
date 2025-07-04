e7b0293f8e8a1252a5f384934b79179650923971
Jonathan <bernardojonathansimon@gmail.com>
2025-07-04 09:45:52 +0800

    renamed mediapipe_pose package

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ros_cart
+
+# ./src/mediapipe_cam
+# edit the IP address to match phone's IP Camera 
--- src/mediapipe_pose/setup.py
+++ src/mediapipe_pose/setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup
 
-package_name = 'mediapipe_pose_ros2'
+package_name = 'mediapipe_pose'
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', [
-            f'resource/{package_name}']),
+        ('share/ament_index/resource_index/packages',
+            [f'resource/{package_name}']),
         (f'share/{package_name}', ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='jsdgbernardo',
+    maintainer='jssgbernardo',
     maintainer_email='bernardojonathansimon@gmail.com',
-    description='ROS 2 wrapper for MediaPipe pose detection',
+    description='MediaPipe-based pose tracker for ROS 2',
     license='License',
     entry_points={
         'console_scripts': [
-            'pose_node = mediapipe_pose_ros2.pose_node:main',
+            'pose_node = mediapipe_pose.pose_node:main',
         ],
     },
 )
